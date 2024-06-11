1656776d4028c79cda0e002c3762b9852cd45a08
77Yashraj <yashrajprogram7@gmail.com>
2024-06-11 14:59:12 +0530

    This File IS related to Function

---
+++ Testfunction.java
@@ -0,0 +1,18 @@
+package project;
+
+public class Testfunction {
+	public static void add() {
+		int a=10;
+		int  b=20;
+		int total=a+b;
+		System.out.println("addtion="+total);
+	}
+	
+	public static void sub(int a ,int b) {
+		int e=10;
+		int  f=20;
+		int total=a-b;
+		System.out.println("subtraction="+total);
+	}
+
+}
---
+++ Testfunction_2.java
@@ -0,0 +1,14 @@
+package project;
+
+public class Testfunction_2 {
+
+	public static void main(String[] args) {
+		//System.out.println("Xyz");
+		Testfunction tf=new Testfunction();
+		tf.add();
+		//Testfuction.add();
+		tf.sub(12,50);
+		
+	}
+
+}
